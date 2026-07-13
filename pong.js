d5cb24ce4083419b61000610370e0ebc7318f94d
Alexander-186 <alexfordjoyce@gmail.com>
2026-07-12 22:50:44 -0400

    Implement player movement with velocity
    Added player velocity for both players and implemented movement controls.

--- pong.js
+++ pong.js
@@ -7,12 +7,14 @@ let context;
 //players
 let playerWidth = 10;
 let playerHeight = 50;
+let playerVelocityY = 0;
 
 let player1 = {
   x : 10,
   y : BoardHeight/2,
   width : playerWidth,
   height : playerHeight
+  velocityY : playerVelocityY
 }
 
 let player2 = {
@@ -20,6 +22,7 @@ let player2 = {
   y : BoardHeight/2,
   width : playerWidth,
   height : playerHeight
+  velocityY : playerVelocityY
 }
 
 window.onload = function() {
@@ -32,7 +35,8 @@ window.onload = function() {
   context.fillStyle = "skyblue";
   context.fillRect(player1.x, player1.y, player1.width, player1.height);
 
-  requestAnimationFrame(update);                          
+  requestAnimationFrame(update);
+  document.addEventListener("keyup", movePlayer);
 }
 
 function update() {
@@ -40,15 +44,34 @@ function update() {
 
   //player 1
   context.fillStyle = "skyblue";
+  player1.y += player1.velocityY;
   context.fillRect(player1.x, player1.y, player1.width, player1.height);
 
   //player 2
+  player2.y += player2.velocityY;
   context.fillRect(player2.x, player2.y, player2.width, player2.height);
   
 }
 
 
+function MovePlayer(e) {
+  //player1
+  if (e.code == "KeyW") {
+    player1.velocityY = -3;
+  }
+  else if (e.code == "Key5") {
+    player1.velocityY = 3;
+  }
 
+  //player2
+  if (e.code == "ArrowUp") {
+    player2.velocityY = -3;
+  }
+  else if (e.code == "ArrowDown") {
+    player2.velocityY = 3;
+  }
+  
+}
 
 
 
